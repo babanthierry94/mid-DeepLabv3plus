29627161c173d3c7c4f1931ad338f6478afaa02a
Thierry BABAN <70677643+babanthierry94@users.noreply.github.com>
2024-01-04 10:39:05 +0100

    update

--- Models/Unet.py
+++ Models/Unet.py
@@ -1,5 +1,5 @@
 #########################################################################################################
-# This code implement the Unet model with VGG16 encoder
+# This code implement the Unet model
 # We reproduced the architecture published in [1].
 #
 # [1] @inproceedings{ronneberger2015u,
@@ -10,14 +10,11 @@
 #  year={2015},
 #  organization={Springer}
 # }
-# https://idiotdeveloper.com/vgg16-unet-implementation-in-tensorflow/
-# https://www.kaggle.com/code/aithammadiabdellatif/vgg16-u-net
-# https://github.com/nikhilroxtomar/Semantic-Segmentation-Architecture/blob/main/TensorFlow/vgg16_unet.py
-# https://github.com/zhoudaxia233/PyTorch-Unet/blob/master/README.md
 # https://arxiv.org/pdf/1505.04597v1.pdf
+# https://github.com/bnsreenu/python_for_microscopists/blob/master/208-simple_multi_unet_model.py
 #########################################################################################################
 
-# VGG16 Unet
+# Unet
 import tensorflow as tf 
 
 def double_conv_block(x, n_filters):
@@ -31,6 +28,11 @@ def double_conv_block(x, n_filters):
     x = tf.keras.layers.Activation("relu")(x)
     return x
 
+def downsample_block(x, n_filters):
+    f = double_conv_block(x, n_filters)
+    p = tf.keras.layers.MaxPool2D(strides=(2,2))(f)
+    return f, p
+
 def upsample_block(x, conv_features, n_filters):
     # upsample
     x = tf.keras.layers.Conv2DTranspose(n_filters, kernel_size = (2,2), strides=(2,2), padding="same")(x)
@@ -40,30 +42,26 @@ def upsample_block(x, conv_features, n_filters):
     x = double_conv_block(x, n_filters)
     return x
 
-def VGG16_Unet(input_shape=(512, 512, 3), num_classes=21):
+def Unet(input_shape=(512, 512, 3), num_classes=21):
     # inputs
     inputs = tf.keras.layers.Input(shape=input_shape)
 
-    vgg16 = tf.keras.applications.VGG16(include_top=False, weights='imagenet', input_tensor = inputs)
-    # vgg16.summary()
-
     # encoder: contracting path - downsample
-    # 1 - downsample 64
-    f1 = vgg16.get_layer("block1_conv2").output
-    print(f1.shape)
-    # 2 - downsample 128
-    f2 = vgg16.get_layer("block2_conv2").output
-    # 3 - downsample 256
-    f3 = vgg16.get_layer("block3_conv3").output
-    # 4 - downsample 512
-    f4 = vgg16.get_layer("block4_conv3").output
+    # 1 - downsample
+    f1, p1 = downsample_block(inputs, 64)
+    # 2 - downsample
+    f2, p2 = downsample_block(p1, 128)
+    # 3 - downsample
+    f3, p3 = downsample_block(p2, 256)
+    # 4 - downsample
+    f4, p4 = downsample_block(p3, 512)
 
-    # 5 - the center 1024
-    center = vgg16.get_layer("block5_conv3").output
+    # 5 - bottleneck
+    bottleneck = double_conv_block(p4, 1024)
 
     # decoder: expanding path - upsample
     # 6 - upsample
-    u6 = upsample_block(center, f4, 512)
+    u6 = upsample_block(bottleneck, f4, 512)
     # 7 - upsample
     u7 = upsample_block(u6, f3, 256)
     # 8 - upsample
@@ -75,9 +73,10 @@ def VGG16_Unet(input_shape=(512, 512, 3), num_classes=21):
     outputs = tf.keras.layers.Conv2D(num_classes, 1, padding="same", activation = "softmax")(u9)
 
     # unet model with Keras Functional API
-    unet_model = tf.keras.Model(inputs, outputs, name="VGG16_UNet")
+    unet_model = tf.keras.Model(inputs, outputs, name="U_Net")
 
     return unet_model
 
-# model = VGG16_Unet(num_classes=10, input_shape=(512, 512, 3))
+
+# model = Unet(num_classes=10, input_shape=(512, 512, 3))
 # model.summary()
